6f9ced1d64be85f0c03647e541b7af534b2d5ffe
Luís Felipe Prates Cattelan <luisfelipe1998@Gmail.com>
2022-04-19 18:45:42 -0300

    debug tqdm progress

--- NN_utils/train_and_eval.py
+++ NN_utils/train_and_eval.py
@@ -247,8 +247,8 @@ class Trainer():
         for e in progress_epoch:
             progress_epoch.set_description(f'Loss: {self.hist_train.loss_list[-1]} | Acc_train: {self.hist_train.acc_list[-1]} | Acc_val: {self.hist_val.acc_list[-1]} \n | Epochs progress:')
             self.epoch += 1
-            progress = tqdm(data,position=0, leave=True)
-            loss = train_NN(self.model,self.optimizer,progress,self.loss_fn,0, print_loss = False) #model.train applied internally here
+            progress = tqdm(data,position=0, leave=False)
+            loss = train_NN(self.model,self.optimizer,progress,self.loss_fn,1, print_loss = False) #model.train applied internally here
             progress.set_description(f'Loss: {loss}')
             self.update_hist()
             if (self.update_lr_epochs>0) and (self.epoch%self.update_lr_epochs == 0):
