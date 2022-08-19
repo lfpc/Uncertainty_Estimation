35d0cdb34a4e0d02a5061e3ba5d6a57aed319be7
Luís Felipe Prates Cattelan <luisfelipe1998@Gmail.com>
2022-08-19 19:56:05 -0300

    .

--- torch_data/__init__.py
+++ torch_data/__init__.py
@@ -176,3 +176,4 @@ class Noisy_DataGenerator(DataGenerator):
         return infos    
 
 from .cifar import Cifar10,Cifar100,Cifar100C,Cifar10C,CIFAR_C_loader
+from .cifar.imbalance import ImbalanceCifar10,ImbalanceCifar100
