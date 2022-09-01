7c77856adda221fc370db516a2529346050bd0df
Luís Felipe Prates Cattelan <luisfelipe1998@Gmail.com>
2022-09-01 15:15:23 -0300

    imagenet cancel get classes

--- torch_data/ImageNet/__init__.py
+++ torch_data/ImageNet/__init__.py
@@ -36,7 +36,7 @@ class ImageNet(DataGenerator):
         if val and test:
             raise Warning("val and test are the same since original test has no labels")
         super().__init__(params, training_data, validation_data, test_data, dataloader)
-        self.classes = self.get_classes(join(data_dir,'imagenet1k_classes.txt'))
+        #self.classes = self.get_classes(join(data_dir,'imagenet1k_classes.txt'))
     def get_classes(self,file = 'imagenet1k_classes.txt'):
         import json
         with open(file) as f:
