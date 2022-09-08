d48fd85aa04ece58a3fd39855f057e89a9578fd8
Luís Felipe Prates Cattelan <luisfelipe1998@Gmail.com>
2022-09-07 22:59:41 -0300

    debug mcbn

--- uncertainty/MCBN.py
+++ uncertainty/MCBN.py
@@ -11,8 +11,7 @@ class MonteCarloBatchNormalization(ensemble.Ensemble):
                       softmax = False, name = 'MCBN'):
         self.as_ensemble = as_ensemble
         super().__init__(model, return_uncs= return_uncs, as_ensemble = as_ensemble, softmax = softmax, name=name)
-        
-        assert isinstance(batch_loader.sampler,torch.utils.data.sampler.RandomSampler), "Batch Loader should have shuffle set to True to give randomness"
+        #assert isinstance(batch_loader.sampler,torch.utils.data.sampler.RandomSampler), "Batch Loader should have shuffle set to True to give randomness"
         self.batch_loader = batch_loader
         self.n_samples = n_samples
 
