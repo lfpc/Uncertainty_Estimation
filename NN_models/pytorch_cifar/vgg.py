ac31f2846bc11ace5c4f00fa9d47651bac7bff1f
Luís Felipe Prates Cattelan <luisfelipe1998@Gmail.com>
2022-09-15 18:06:09 -0300

    debug vgg

--- NN_models/pytorch_cifar/vgg.py
+++ NN_models/pytorch_cifar/vgg.py
@@ -34,9 +34,10 @@ class VGG(nn.Module):
                 layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding='same'),
                            nn.BatchNorm2d(x),
                            nn.ReLU(inplace=True)]
+                in_channels = x
             if conv_drop:
                 layers += nn.Dropout(self.drop_rate)
-            in_channels = x
+            
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
         return nn.Sequential(*layers)
 
