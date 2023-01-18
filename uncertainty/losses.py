5f38ca517cf9450da81aaf30a2ea3f18c0e54189
Luís Felipe Prates Cattelan <luisfelipe1998@Gmail.com>
2023-01-18 17:14:57 -0300

    SelectiveNet Loss

--- uncertainty/losses.py
+++ uncertainty/losses.py
@@ -225,140 +225,53 @@ class penalized_uncertainty(torch.nn.Module):
         with torch.no_grad():
             self.L0 = new_L0
 
-def entropy_const(w):
-    H = torch.exp(entropy(w,reduction = 'sum'))/w.size(0)
-    return H
-normalize_tensor = (lambda x,dim=-1: F.normalize(x, p=1,dim=dim))
-
-def IPM_selectivenet(r,const,lamb = 32):
-    #optimize x such that const >0 with quadratic penalty
-    gama = lamb*torch.square(torch.maximum(torch.tensor([0]).cuda(),const))
-    objective = r + gama
-    return objective
-
-def IPM_log(r,const,t = 32):
-    #optimize x such that const >0 with log barrier
-    gama = -(1/t)*torch.log(-const)
-    return r+gama
-def IPM_log_adap(r,const, t = 32):
-    if const <= -(1/(t**2)):
-        gama = IPM_log(r,const,t)
-    else:
-        gama = t*const-(1/t)*log(1/(t**2))+(1/t)
-    return r+gama
-
-
-
-class selective_net_2(torch.nn.Module):
-    def __init__(self,criterion,w_fn = normalize_tensor,c_fn = torch.mean,optim_method = IPM_selectivenet, c = 0.8,
-                 alpha = 1.0, head = None,const_var = 'g'):
+class SelectiveNetLoss(torch.nn.Module):
+    def __init__(self,criterion = torch.nn.CrossEntropyLoss(reduction='none'), c = 0.8, lamb = 32,
+                 alpha = 1.0, aux_head = 'aux',reduction = 'mean'):
         super().__init__()
 
         self.criterion = criterion #criterion must have reduction set to 'none'
-        self.w_fn = w_fn #transform applied to g
-        self.c_fn = c_fn #transform applied to w that goes onto constraint
-        self.optim_method = optim_method #transform applied to risk (loss) and constraint and returns a equivalent unconstrained objective
+        try:
+            self.criterion.reduction = 'none'
+        except: pass
+        self.lamb = lamb
         self.c = c #coverage
         self.alpha = alpha
-        self.head = head
-        self.const_var = const_var
-    
-    def get_loss(self,y_pred,w,y_true):
-        
-        loss = w*self.criterion(y_pred,y_true)
-        loss = torch.sum(loss) #sum? mean? When w is a normalization, is must be sum. How to generalize?
+        self.reduction = reduction
+        self.aux_head = aux_head
 
+    def lagrangian(self,const):
+        #variant of the well-known Interior Point Method (IPM)
+        return self.lamb*torch.square(torch.nn.functional.relu(const))
+    
+    def selective_risk(self,y_pred,y_true,g):
+        
+        loss = self.criterion(y_pred,y_true)*g
+        loss /= g.mean()
+        if self.reduction == 'mean':
+            loss = loss.mean()
+        elif self.reduction == 'sum':
+            loss = loss.sum()
         return loss
 
-    def get_constraint(self,w): 
-        H = self.c_fn(w) #must be >= c
-        constraint = self.c - H #must be <=0
-        return constraint
+    def constraint(self,g): 
+        return self.c - g.mean() #must be <=0
 
     def forward(self,output,y_true):
         
-        y_pred,g = output
+        if self.aux_head == 'self':
+            y_pred,g = output
+            y_h = y_pred
+        elif self.aux_head == 'aux':
+            y_pred,g,y_h = output
+            
         g = g.view(-1)
-        w = self.w_fn(g)
         
-        loss = self.get_loss(y_pred,w,y_true)
-        if self.optim_method is not None:
-            if self.const_var == 'w':
-                const = self.get_constraint(w)
-            elif self.const_var == 'g':
-                const = self.get_constraint(g)
-            loss = self.optim_method(loss, const)
+        loss = self.selective_risk(y_pred,g,y_true)
+        loss += self.lagrangian(self.constraint(g))
 
-        if self.head is None:
-            loss_h = 0
-        else:
-            w = self.w_fn(torch.ones([torch.numel(g)]),dim=-1).to(y_pred.device)
-            if self.head == 'y':
-                loss_h = self.get_loss(y_pred,w,y_true)
-            else: 
-                h = self.head()
-                loss_h = self.get_loss(h,w,y_true)
+        loss_h = self.selective_risk(y_h,g,y_true)
         loss = self.alpha*loss + (1-self.alpha)*loss_h
-
-        return loss
-
-def w_fn(g,lamb = 1):
-    lamb = torch.as_tensor(lamb)
-    w = torch.nn.softmax(lamb*g,dim=-1)
-    return w
-def H_fn(g,lamb = 1):
-    w = w_fn(g,lamb)
-    H = entropy(w)
-    return H
-def H_const(lamb,*args):
-    g,c = args
-    H = H_fn(g,lamb)
-    const = (torch.exp(H)/torch.numel(g))-c
-    return const
-
-class selective_net_lambda(torch.nn.Module):
-    def __init__(self,criterion,w_fn = w_fn, c_fn = H_const,c = 0.8,alpha = 1.0, head = None):
-        super().__init__()
-
-        self.criterion = criterion #criterion must have reduction set to 'none'
-        self.w_fn = w_fn #transform applied to g
-        self.c_fn = c_fn #transform applied to w that goes onto constraint
-        self.c = c #coverage
-        self.alpha = alpha
-        self.head = head
-        self.lamb = 1
-
-    
-    def get_loss(self,y_pred,w,y_true):
-        
-        loss = w*self.criterion(y_pred,y_true)
-        loss = torch.sum(loss) #sum? mean? When w is a normalization, is must be sum. How to generalize?
-        return loss
-
-    def update_lambda(self,g):
-        self.lamb = root_scalar(self.c_fn,bracket=[0, 100],args = (g,self.c)).root
-        return self.lamb
-
-    def forward(self,output,y_true):
-        
-        y_pred,g = output
-        g = g.view(-1)
-        self.update_lambda(g)
-        w = self.w_fn(g,self.lamb)
-        
-        loss = self.get_loss(y_pred,w,y_true)
-
-        if self.head is None:
-            loss_h = 0
-        else:
-            w = self.w_fn(torch.ones([torch.numel(g)]),dim=-1).to(y_pred.device)
-            if self.head == 'y':
-                loss_h = self.get_loss(y_pred,w,y_true)
-            else: 
-                h = self.head()
-                loss_h = self.get_loss(h,w,y_true)
-        loss = self.alpha*loss + (1-self.alpha)*loss_h
-
         return loss
 
 class confid_loss(torch.nn.Module):
