8d87e4ed671551163bb9862a5120352be670efe1
Luís Felipe Prates Cattelan <luisfelipe1998@Gmail.com>
2023-02-02 19:53:09 -0300

    ECE in metrics

--- uncertainty/calibration.py
+++ uncertainty/calibration.py
@@ -7,49 +7,6 @@ from uncertainty import get_MCP
 from NN_utils.train_and_eval import correct_class
 
 
-'''From https://github.com/gpleiss/temperature_scaling/blob/master/temperature_scaling.py :'''
-class _ECELoss(torch.nn.Module):
-    """
-    Calculates the Expected Calibration Error of a model.
-    (This isn't necessary for temperature scaling, just a cool metric).
-    The input to this loss is the logits of a model, NOT the softmax scores.
-    This divides the confidence outputs into equally-sized interval bins.
-    In each bin, we compute the confidence gap:
-    bin_gap = | avg_confidence_in_bin - accuracy_in_bin |
-    We then return a weighted average of the gaps, based on the number
-    of samples in each bin
-    See: Naeini, Mahdi Pakdaman, Gregory F. Cooper, and Milos Hauskrecht.
-    "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
-    2015.
-    """
-    def __init__(self, n_bins=15, softmax = False):
-        """
-        n_bins (int): number of confidence interval bins
-        """
-        super(_ECELoss, self).__init__()
-        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
-        self.bin_lowers = bin_boundaries[:-1]
-        self.bin_uppers = bin_boundaries[1:]
-        self.SM = softmax
-
-    def forward(self, y, labels):
-        if self.SM:
-            y = softmax(y, dim=1)
-        confidences, predictions = torch.max(y, 1)
-        accuracies = predictions.eq(labels)
-
-        ece = torch.zeros(1, device=y.device)
-        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
-            # Calculated |confidence - accuracy| in each bin
-            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
-            prop_in_bin = in_bin.float().mean()
-            if prop_in_bin.item() > 0:
-                accuracy_in_bin = accuracies[in_bin].float().mean()
-                avg_confidence_in_bin = confidences[in_bin].mean()
-                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
-
-        return ece
-
 class Binning():
     def __init__(self,p,hits,n = 10, bounds = (0,1),division = 'normal'):
         
--- uncertainty/metrics.py
+++ uncertainty/metrics.py
@@ -150,22 +150,49 @@ def calibration_curve(
     else:    
         return prob_true, prob_pred
 
-def ECE(y_true,
-    y_prob,
-    normalize=False,
-    n_bins=5,
-    strategy="uniform"):
-
-    _,_, ece = calibration_curve(
-    y_true,
-    y_prob,
-    normalize,
-    n_bins,
-    strategy,
-    return_ece = True)
+class ECE(torch.nn.Module):
     
-    return ece
+    '''From https://github.com/gpleiss/temperature_scaling/blob/master/temperature_scaling.py :'''
+    """
+    Calculates the Expected Calibration Error of a model.
+    (This isn't necessary for temperature scaling, just a cool metric).
+    The input to this loss is the logits of a model, NOT the softmax scores.
+    This divides the confidence outputs into equally-sized interval bins.
+    In each bin, we compute the confidence gap:
+    bin_gap = | avg_confidence_in_bin - accuracy_in_bin |
+    We then return a weighted average of the gaps, based on the number
+    of samples in each bin
+    See: Naeini, Mahdi Pakdaman, Gregory F. Cooper, and Milos Hauskrecht.
+    "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
+    2015.
+    """
+    def __init__(self, n_bins=10, softmax = False):
+        """
+        n_bins (int): number of confidence interval bins
+        """
+        super(ECE, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+        self.SM = softmax
 
+    def forward(self, y, labels):
+        if self.SM:
+            y = torch.nn.functional.softmax(y, dim=1)
+        confidences, predictions = torch.max(y, 1)
+        accuracies = predictions.eq(labels)
+
+        ece = torch.zeros(1, device=y.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+        return ece
 
 
 
